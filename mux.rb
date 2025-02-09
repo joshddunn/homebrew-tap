8e95639784a049cb82de1c765954673f6a050490
Josh Dunn <joshdunn.uwo@gmail.com>
2025-02-09 02:09:18 -0500

    update tap formula

--- mux.rb
+++ mux.rb
@@ -1,6 +1,4 @@
-# https://docs.brew.sh/Cask-Cookbook
-
-cask "mux" do
+class Mux < Formula
   version "v0.1.0"
   sha256 "c73b2815494508d75987b310305072ab3996ce138fbe37ad0860373998d9f8c1"
 
@@ -9,5 +7,7 @@ cask "mux" do
   desc "Configure tmux sessions"
   homepage "https://github.com/joshddunn/mux"
 
-  binary "mux"
+  def install
+    bin.install "mux"
+  end
 end
