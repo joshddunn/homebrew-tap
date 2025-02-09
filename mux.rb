7317beaeb716623ac56646c41087df9b8c1de4a9
Josh Dunn <joshdunn.uwo@gmail.com>
2025-02-09 02:17:53 -0500

    update sha

--- mux.rb
+++ mux.rb
@@ -1,6 +1,6 @@
 class Mux < Formula
   version "v0.1.0"
-  sha256 "c73b2815494508d75987b310305072ab3996ce138fbe37ad0860373998d9f8c1"
+  sha256" b3637495314201cbea6266ff8d1b4c0f6a7fd8390e20f0a5683f6430249d0fa4"
 
   url "https://github.com/joshddunn/mux/archive/refs/tags/#{version}.tar.gz"
   name "mux"
