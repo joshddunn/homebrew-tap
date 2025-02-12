7c5d2f52dfce9b72a14a4dd6658df9cedcc976f8
Josh Dunn <joshdunn.uwo@gmail.com>
2025-02-11 21:35:29 -0500

    v0.1.1

--- mux.rb
+++ mux.rb
@@ -1,6 +1,6 @@
 class Mux < Formula
-  version "v0.1.0"
-  sha256 "a94685619f4c15c23203705d152821c361c73e0e4671767a8ab5b53e1932db4c"
+  version "v0.1.1"
+  sha256 "0e66b773cfbf4871b00240f291413311375fef356c53adb471f68b15d6a089a6"
 
   url "https://github.com/joshddunn/mux/archive/refs/tags/#{version}.tar.gz"
   desc "Configure tmux sessions"
