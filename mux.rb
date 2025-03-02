c88e3953ea89d1af0805360bace81e7033b75d5c
Josh Dunn <joshdunn.uwo@gmail.com>
2025-03-01 23:50:47 -0500

    update sha

--- mux.rb
+++ mux.rb
@@ -1,6 +1,6 @@
 class Mux < Formula
   version "v0.1.5"
-  sha256 "267a60c34c78a9e0f03f1f489fac62f2f5220e5f736da160f328b4b3106b3f3a"
+  sha256 "7f6c36e585b694fedacf5f65a16af40a3b249d1982668c8e7384dd3c2e390ae1"
 
   url "https://github.com/joshddunn/mux/releases/download/#{version}/mux"
   desc "Manage tmux sessions"
