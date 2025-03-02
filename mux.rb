d23e92c7a9e9446ff465454363983e6b92c5a6ad
Josh Dunn <joshdunn.uwo@gmail.com>
2025-03-02 00:23:01 -0500

    different build approach

--- mux.rb
+++ mux.rb
@@ -1,13 +1,19 @@
 class Mux < Formula
   version "v0.1.5"
-  sha256 "0419f98d00425b6967cf3ba1b603ddb90e1b1a02dcd8fdfbd6f6185b6f722aeb"
 
-  url "https://github.com/joshddunn/mux/releases/download/#{version}/mux"
+  url "https://github.com/joshddunn/mux/archive/refs/tags/#{version}.tar.gz"
   desc "Manage tmux sessions"
   homepage "https://github.com/joshddunn/mux"
 
+  depends_on "go" => :build
+
   def install
     bin.install "mux"
-    # generate_completions_from_executable(bin/"mux", "completion", shells: [:zsh])
+    ldflags = %W[
+      -X main.version=#{version}
+    ]
+
+    system "go", "build", "-o", "mux", *std_go_args(ldflags: ldflags)
+    generate_completions_from_executable(bin/"mux", "completion", shells: [:zsh])
   end
 end
